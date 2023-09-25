6a1553f1148438e4385a2e19f25232704c642f8b
nabilwafi <nabilwafi88@gmail.com>
2023-09-26 00:10:37 +0700

    Major Update

--- SayHello.go
+++ SayHello.go
@@ -1,7 +1,5 @@
 package go_say_hello
 
-import "fmt"
-
-func SayHello() {
-	fmt.Println("Hello, Friend!")
+func SayHello() string {
+	return "Hello, World!"
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/testingnabilwafi/go-say-hello
+module github.com/testingnabilwafi/go-say-hello/v2
 
 go 1.19
